ec4810bcd067bc621f02a6a65c306678499d8338
Daniel Wagner <daniel@wagners.name>
2021-12-07 14:02:36 +0100

    Reimplemented day 1, puzzle 1 and 2 in C#

---
+++ Day1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("AoC 2017 Day 1");
+
+var input = (await File.ReadAllLinesAsync("input.txt")).First().Select(c => c - '0').ToArray();
+
+Console.WriteLine($"One: {PuzzleOne(input)}");
+Console.WriteLine($"Two: {PuzzleTwo(input)}");
+
+int PuzzleOne(int[] input)
+{
+	return input.Select((d, i) => (d, i)).Where(d => (d.d == input[(d.i + 1) % input.Length])).Sum(d => d.Item1);
+}
+
+int PuzzleTwo(int[] input)
+{
+	return input.Select((d, i) => (d, i)).Where(d => (d.d == input[(d.i + input.Length / 2) % input.Length])).Sum(d => d.Item1);
+}
